7f0dc59754d181b4afe46aa33264fc1716731caa
Shriraj Patil <91754780+shrirajpatil@users.noreply.github.com>
2025-09-21 21:26:02 +0530

    Update DBConnectionUtil.java

--- assessment11_banking_app_with_jdbc-master/src/main/java/com/hdfc/minibank/util/DBConnectionUtil.java
+++ assessment11_banking_app_with_jdbc-master/src/main/java/com/hdfc/minibank/util/DBConnectionUtil.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnectionUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/minibank";
-    private static final String USER = "postgres";      // your DB user
-    private static final String PASSWORD = "Mrunali2003";  // your DB password
+    private static final String USER = "admin";      // your DB user
+    private static final String PASSWORD = "admin";  // your DB password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
